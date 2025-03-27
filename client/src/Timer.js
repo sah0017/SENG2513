83cd8f25c6e4bd45d51f69d1bbbc52d2b605685c
eshanks <ethanhanks456@gmail.com>
2025-03-27 13:10:46 -0500

    added timer functionality

--- client/src/Timer.js
+++ client/src/Timer.js
@@ -1,11 +1,52 @@
-import "./Timer.css"
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Timer.css";
 
 const Timer = () => {
+    const [isRunning, setIsRunning] = useState(false);
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        let interval;
+        if (isRunning) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 10);
+            }, 10);
+        } else {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [isRunning]);
+
+    const hours = Math.floor(time / 3600000);
+    const minutes = Math.floor((time % 3600000) / 60000);
+    const seconds = Math.floor((time % 60000) / 1000);
+    const milliseconds = Math.floor((time % 1000) / 10);
+
+    const handleStart = () => setIsRunning(true);
+    const handleStop = () => setIsRunning(false);
+    const handleReset = () => {
+        setIsRunning(false);
+        setTime(0);
+    };
+
     return (
-        <>
-            <h1>This is where our timer goes</h1>
-        </>
+        <div className="container">
+            <h1>Stopwatch</h1>
+            <div id="time">
+                <span className="digit">{String(hours).padStart(2, "0")}</span>
+                <span className="txt">Hr</span>
+                <span className="digit">{String(minutes).padStart(2, "0")}</span>
+                <span className="txt">Min</span>
+                <span className="digit">{String(seconds).padStart(2, "0")}</span>
+                <span className="txt">Sec</span>
+                <span className="digit">{String(milliseconds).padStart(2, "0")}</span>
+            </div>
+            <div id="buttons">
+                <button className="btn" onClick={handleStart}>Start</button>
+                <button className="btn" onClick={handleStop}>Stop</button>
+                <button className="btn" onClick={handleReset}>Reset</button>
+            </div>
+        </div>
     );
 };
 
