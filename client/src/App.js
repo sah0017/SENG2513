5e62ca9565302f731dbaaee40739355c5b8f0402
ryanvanderstelt <ryan@vstelt.com>
2025-03-27 12:16:56 -0500

    unformatted sections added to App.js, placeholder format and html for each section

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 // src/App.js
 import "./App.css";
 import Navbar from "./Navbar";
+import Timer from "./Timer"
+import Notes from "./Notes"
+import Guides from "./Guides"
 
 function App() {
   return (
     <>
       <Navbar />
-
+      <Notes />
+      <Timer />
+      <Guides />
     </>
   );
 }
---
+++ client/src/Timer.js
@@ -0,0 +1,12 @@
+import "./Timer.css"
+import React from "react";
+
+const Timer = () => {
+    return (
+        <>
+            <h1>This is where our timer goes</h1>
+        </>
+    );
+};
+
+export default Timer;
